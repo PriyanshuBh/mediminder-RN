6d867a1faf5ba95ca22770a52ee7a34026f81f93
PriyanshuBh <priyanshubh2003@gmail.com>
2024-12-18 17:15:26 +0530

    constants

---
+++ constant/ConstantString.jsx
@@ -0,0 +1,6 @@
+export default{
+    NoMedication:'No Mediciations!',
+    MedicationSubText:'You have 0 medidication setup, Kindly setup a new one',
+    AddNewMedicationBtn:'+ Add New Medication'
+}
+
