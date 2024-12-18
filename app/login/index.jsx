d7fc4ac53d245f680c30354a461b144b5e3344c1
PriyanshuBh <priyanshubh2003@gmail.com>
2024-12-18 17:15:17 +0530

    login

---
+++ app/login/index.jsx
@@ -0,0 +1,69 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import Colors from "../../constant/Colors";
+import { useRouter } from "expo-router";
+
+ export default function LoginScreen() {
+  const router = useRouter();
+  return (
+    <View>
+      <View style={{ display: "flex", alignItems: "center", marginTop: 20 }}>
+        <Image
+          source={require("./../../assets/images/login.png")}
+          style={styles?.image}
+        />
+      </View>
+      <View
+        style={{
+          padding: 25,
+          backgroundColor: Colors.PRIMARY,
+          height: "34%",
+          borderTopLeftRadius: 40,
+          borderTopRightRadius: 40,
+          zIndex:2,
+          marginTop: 0
+        }}
+      >
+        <Text style={{
+                fontSize: 30,
+                fontWeight: 'bold',
+                color: "white",
+                textAlign: "center"
+        }}>Stay on Track, Stay Healthy</Text>
+        <Text style={{
+          color: "white",
+          textAlign: "center",
+          fontSize: 14,
+          marginTop:  20
+        }}>
+          Track your meds, take control of your health and live a healthier life.
+        </Text>
+
+        <TouchableOpacity style={styles?.button} onPress={()=> router.push("/login/signIn")}>
+          <Text style={{
+            color: Colors.PRIMARY,
+            textAlign: "center",
+            fontSize: 16
+          }}>Continue</Text>
+        </TouchableOpacity>
+        <Text style={{color: "white", textAlign: "center", marginTop: 20 , fontSize: 12}}>
+          Note: By Clicking Continue button, you agree to our Terms and Conditions.
+        </Text>
+      </View> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: "66%",
+    borderRadius: 24,
+  },
+  button:{
+    padding:15,
+    backgroundColor: "white",
+    borderRadius: 99,
+    marginTop: 25
+  }
+});
---
+++ app/login/signUp.jsx
@@ -0,0 +1,151 @@
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ToastAndroid,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import Colors from "../../constant/Colors";
+import { useRouter } from "expo-router";
+import { auth } from "../../config/FirebaseConfig";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { setLocalStorage } from "../../service/Storage";
+
+export default function SignUp() {
+  const router = useRouter();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [userName, setUserName] = useState();
+
+  const OnCreateAccount = () => {
+    if (!email || !password || !userName) {
+      ToastAndroid.show("Please enter email and password", ToastAndroid.BOTTOM);
+      Alert.alert("Please enter email and password");
+
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        await updateProfile(user, {
+          displayName: userName
+        })
+
+        await setLocalStorage("userDetail", user);
+        router.replace("(tabs)")
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        if (errorCode === "auth/email-already-in-use") {
+          ToastAndroid.show("Email already exists", ToastAndroid.BOTTOM);
+          Alert.alert("Email already exists");
+        }
+        // ..
+      });
+  };
+  return (
+    <View
+      style={{
+        padding: 20,
+      }}
+    >
+      <Text style={styles.textHeader}>Create New Account</Text>
+
+      <View
+        style={{
+          marginTop: 25,
+        }}
+      >
+        <Text>Full Name</Text>
+        <TextInput placeholder="Full Name" style={styles.textInput} onChangeText={(value) => setUserName(value)} />
+      </View>
+
+      <View
+        style={{
+          marginTop: 25,
+        }}
+      >
+        <Text>Email</Text>
+        <TextInput
+          placeholder="Email"
+          style={styles.textInput}
+          onChangeText={(value) => setEmail(value)}
+        />
+      </View>
+
+      <View
+        style={{
+          marginTop: 25,
+        }}
+      >
+        <Text>Password</Text>
+        <TextInput
+          placeholder="Password"
+          style={styles.textInput}
+          secureTextEntry={true}
+          onChangeText={(value) => setPassword(value)}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={OnCreateAccount}>
+        <Text style={{ color: "white", textAlign: "center", fontSize: 17 }}>
+          Create Account
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.buttonCreate}
+        onPress={() => router.push("/login/signIn")}
+      >
+        <Text
+          style={{ color: Colors.PRIMARY, textAlign: "center", fontSize: 17 }}
+        >
+          Already have an account? Sign In
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  textHeader: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 15,
+  },
+  subText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: Colors.GRAY,
+    marginTop: 10,
+  },
+  textInput: {
+    padding: 10,
+    borderWidth: 1,
+    fontSize: 17,
+    borderRadius: 10,
+    marginTop: 10,
+    backgroundColor: "white",
+  },
+  button: {
+    padding: 15,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius: 10,
+    marginTop: 35,
+  },
+  buttonCreate: {
+    padding: 15,
+    backgroundColor: "white",
+    borderRadius: 10,
+    marginTop: 35,
+    borderWidth: 1,
+    borderColor: Colors.PRIMARY,
+  },
+});
